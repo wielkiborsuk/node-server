966232d48c8449a39f85d9ffcf1777243ac70b45
wielki.borsuk <wielki.borsuk@gmail.com>
2013-12-20 16:36:44 +0100

    grouping of require with config of each middleware, removing of http proxy

--- app.js
+++ app.js
@@ -1,48 +1,26 @@
 var express = require('express'),
 	fs = require('fs'),
-	util = require('./util'),
-	httpProxy = require('http-proxy'),
-	chats = require('chat-server'),
-	player = require('player-server'),
-	upload = require('jquery-file-upload-middleware');
+	util = require('./util');
 
 var app = express();
-var https_opts = {
-	key: fs.readFileSync('/home/borsuk/expr/security/server.key'),
-	cert: fs.readFileSync('/home/borsuk/expr/security/server.crt')
-}
 
 var users = {
 	'borsuk': 'pass',
 	'foo': 'foo'
 }
 
-//var proxy = new httpProxy.RoutingProxy();
-var proxy = new httpProxy.HttpProxy({
-	target: {
-		host: '127.0.0.1',
-		port: 3000,
-		https: https_opts
-	},
-	source: {
-		host: '127.0.0.1',
-		port: 3000,
-		https: https_opts
-	},
-	https: https_opts
-});
-
-upload.configure({
-	uploadDir: '../web/upload',
-	uploadUrl: '/www/upload',
-})
-
 app.configure(function () {
 	app.use(express.favicon())
-	app.use('/upload', upload.fileHandler());
-	app.use('/term', function (req, res) {
-		proxy.proxyRequest(req, res);
-	});
+
+  //jquery file upload handler - didn't test it but it seems it has a problem if put to late in handler chain
+  try {
+    var upload = require('jquery-file-upload-middleware');
+    upload.configure({
+      uploadDir: '../web/upload',
+      uploadUrl: '/www/upload',
+    })
+    app.use('/upload', upload.fileHandler());
+  } catch (e) { }
 	app.use('/www', express.basicAuth(function (user, pass) {
 		return user in users && users[user] == pass;
 	}));
@@ -51,8 +29,8 @@ app.configure(function () {
 	app.use(express.cookieParser())
 	app.use('/www', express.static(util.wwwbase))
 	app.use('/www', express.directory(util.wwwbase, {icons:true}))
-    // should be able to add self-made modules here 
-    
+  // should be able to add self-made modules here 
+
 	app.all('*', function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -63,8 +41,18 @@ app.configure(function () {
 			next();
 		}
 	})
-	app.use('/chat', chats({}))
-	app.use('/player', player({basepath:'../web', defaultpath:'/upload/'}))
+
+  //chat-server - backend for simple chat engine, used to try out the publishing methods in angular
+  try {
+    var chats = require('chat-server');
+    app.use('/chat', chats({}))
+  } catch (e) { }
+
+  //player-server - server part for angular audio player, easy private streaming
+  try {
+    var player = require('player-server');
+    app.use('/player', player({basepath:'../web', defaultpath:'/upload/'}))
+  } catch (e) { }
 	
 	app.use(function (req, res) {
 		res.send(404)
